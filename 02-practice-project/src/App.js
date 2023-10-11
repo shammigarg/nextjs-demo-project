d39abd8e4aa8a62dea19d625c2d9b08ce99e8157
Sonu Singhal <sonusinghal@192.168.1.3>
2023-10-11 11:03:12 +0530

    practice project done

--- 02-practice-project/src/App.js
+++ 02-practice-project/src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import AddUser from './Components/AddUser';
+import UsersList from './Components/UsersList';
 
 
 
 function App() {
+  const [enteredUsersData, setEnterUsersData] = useState([])
+
+  const userDataHandler =  (usersData)=>{
+    setEnterUsersData((prevData)=>{
+      return [...prevData,...usersData]
+    })
+    console.log(enteredUsersData)
+
+  }
+   
   return (
     <div>
-      <AddUser />
+      <AddUser onSubmitData={userDataHandler}/>
+      <UsersList users={enteredUsersData}/>
     </div>
   );
 }
