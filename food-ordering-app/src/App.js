3beff6b971d159b23f163ad3517c143366ce7f54
Sonu Singhal <sonusinghal@192.168.1.14>
2023-10-12 22:11:38 +0530

    Meals item added

--- food-ordering-app/src/App.js
+++ food-ordering-app/src/App.js
@@ -1,10 +1,16 @@
-import React,{Fragment} from 'react';
+import React, { Fragment } from 'react';
 import Header from './Components/Layout/Header'
+
+import Meals from './Components/Meals/Meals';
 
 function App() {
   return (
     <Fragment>
-      <Header/>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+
     </Fragment>
   );
 }
--- food-ordering-app/src/Components/Layout/Header.js
+++ food-ordering-app/src/Components/Layout/Header.js
@@ -1,4 +1,4 @@
-import React,{Fragment} from "react";
+import React, { Fragment } from "react";
 
 import HeaderCartButton from "./HeaderCartButton";
 
@@ -6,15 +6,15 @@ import mealImage from "../../assets/meals.jpg"
 
 import classes from "./Header.module.css"
 
-const Header = ()=>{
+const Header = () => {
     return <Fragment>
         <header className={classes.header}>
             <h1>ReactMeals</h1>
-            <HeaderCartButton/>
+            <HeaderCartButton />
         </header>
 
         <div className={classes['main-image']}>
-            <img src={mealImage} alt="A table full of delicious food!"/>
+            <img src={mealImage} alt="A table full of delicious food!" />
         </div>
 
     </Fragment>
